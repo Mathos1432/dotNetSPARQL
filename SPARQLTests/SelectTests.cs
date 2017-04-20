967ec092a1a91694a2084b80a2451dd89dc83c4e
Mathieu Tremblay <mathieu-4.tremblay@polymtl.ca>
2017-04-20 13:44:01 -0400

    initial commit

---
+++ SPARQLTests/SelectTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using dotNetSPARQL;
+
+namespace SPARQLTests
+{
+    [TestClass]
+    public class SelectTests
+    {
+        [TestMethod]
+        public void ValidSELECTObjectVariable()
+        {
+            var subject = new Uri("http://dbpedia.org/resource/Paris");
+            var predicate = new Uri("http://dbpedia.org/ontology/mayor");
+            var objVariable = "uri";
+            var expectedQuery = "SELECT DISTINCT ?uri WHERE { <http://dbpedia.org/resource/Paris> <http://dbpedia.org/ontology/mayor> ?uri }";
+            var queryBuilder = new SPARQL();
+            var query = queryBuilder.SELECT(subject, predicate, objVariable, -1);
+            Assert.AreEqual(expectedQuery, query);
+        }
+    }
+}
---
+++ dotNetSPARQL/SPARQL.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNetSPARQL
+{
+    public class SPARQL
+    {
+        const int DEFAULT_LIMIT = 100;
+
+        /// <summary>
+        /// Returns a Sparql SELECT query with the variable in the object location.
+        /// </summary>
+        /// <param name="subject"></param>
+        /// <param name="predicate"></param>
+        /// <param name="variableName"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        public string SELECT(Uri subject, Uri predicate, string variableName, int limit = DEFAULT_LIMIT)
+        {
+            var requestString = "SELECT DISTINCT ?" + variableName + " WHERE { ";
+            requestString += FormatUri(subject) + " " + FormatUri(predicate) + " ?" + variableName;
+            requestString += " }";
+            return requestString;
+        }
+
+        /// <summary>
+        /// Returns a Sparql SELECT query with the variable in the predicate location.
+        /// </summary>
+        /// <param name="variableName"></param>
+        /// <param name="predicate"></param>
+        /// <param name="obj"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        public string SELECT(Uri subject, string variableName, Uri obj, int limit = DEFAULT_LIMIT)
+        {
+            var requestString = "SELECT DISTINCT ?" + variableName + " WHERE { ";
+            requestString += FormatUri(subject) + " ?" + variableName + " " + FormatUri(obj);
+            requestString += " }";
+            return requestString;
+        }
+
+        /// <summary>
+        /// Returns a Sparql SELECT query with the variable in the subject location.
+        /// </summary>
+        /// <param name="variableName"></param>
+        /// <param name="predicate"></param>
+        /// <param name="obj"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        public string SELECT(string variableName, Uri predicate, Uri obj, int limit = DEFAULT_LIMIT)
+        {
+            var requestString = "SELECT DISTINCT ?" + variableName + " WHERE { ";
+            requestString += "?" + variableName + " " + FormatUri(predicate) + " " + FormatUri(obj);
+            requestString += " }";
+            return requestString;
+        }
+
+        /// <summary>
+        /// Returns a Sparql SELECT query with the variable in the subject location.
+        /// </summary>
+        /// <param name="variableName"></param>
+        /// <param name="predicate"></param>
+        /// <param name="obj"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        public string SELECT(string subjectVariable, Uri predicate, string objectVariable, int limit = DEFAULT_LIMIT)
+        {
+            var requestString = "SELECT DISTINCT ?" + subjectVariable + ", ?" + objectVariable + " WHERE { ";
+            requestString += "?" + subjectVariable + " " + FormatUri(predicate) + " ?" + objectVariable;
+            requestString += " }";
+            return requestString;
+        }
+
+        /// <summary>
+        /// Returns a Sparql SELECT query with the variable in the subject location.
+        /// </summary>
+        /// <param name="variableName"></param>
+        /// <param name="predicate"></param>
+        /// <param name="obj"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        public string SELECT(Uri subject, string predicateVariable, string objectVariable, int limit = DEFAULT_LIMIT)
+        {
+            var requestString = "SELECT DISTINCT ?" + predicateVariable + ", ?" + objectVariable + " WHERE { ";
+            requestString += FormatUri(subject) + " ?" + predicateVariable + " ?" + objectVariable;
+            requestString += " }";
+            return requestString;
+        }
+
+        /// <summary>
+        /// Returns a Sparql SELECT query with the variable in the subject location.
+        /// </summary>
+        /// <param name="variableName"></param>
+        /// <param name="predicate"></param>
+        /// <param name="obj"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        public string SELECT(string subjectVariable, string predicateVariable, Uri obj, int limit = DEFAULT_LIMIT)
+        {
+            var requestString = "SELECT DISTINCT ?" + subjectVariable + ", ?" + predicateVariable + " WHERE { ";
+            requestString += "?" + subjectVariable + " ?" + predicateVariable + " " + FormatUri(obj);
+            requestString += " }";
+            return requestString;
+        }
+
+        private string FormatUri(Uri uri)
+        {
+            return "<" + uri.OriginalString + ">";
+        }
+    }
+}
